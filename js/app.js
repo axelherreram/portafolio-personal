5d4822b49e883e66790436119bbe15596a235626
axelherreram <aherreram22@miumg.edu.gt>
2024-10-06 21:25:39 -0600

    Ajuste global de estilos, modificaciones a la sección de proyectos  y creación de pagina extra para mostrar mas proyectos

--- js/app.js
+++ js/app.js
@@ -6,3 +6,28 @@ menuToggle.addEventListener("click", () => {
   mobileMenu.classList.toggle("hidden");
 });
 
+// Resaltar la sección activa en el navbar
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-link");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+
+    if (pageYOffset >= sectionTop - sectionHeight / 3) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach((link) => {
+    link.classList.remove("text-white", "bg-gray-700"); // Elimina el estilo de la clase
+    if (link.getAttribute("data-target") === current) {
+      link.classList.add("text-[#38bdf8]", "bg-gray-700"); // Añade el estilo para la sección activa
+    }
+  });
+});
+
+
