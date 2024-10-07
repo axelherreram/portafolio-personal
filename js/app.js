e3ff1a18cbee9f137c923c5f754efe1cf62a579a
axelherreram <aherreram22@miumg.edu.gt>
2024-10-06 21:02:13 -0600

    inicio del portafolio, creación de navbar, home y sección de portafolios

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// Script para manejar el botón de abrir/cerrar menú
+const menuToggle = document.getElementById("menu-toggle");
+const mobileMenu = document.getElementById("mobile-menu");
+
+menuToggle.addEventListener("click", () => {
+  mobileMenu.classList.toggle("hidden");
+});
+
