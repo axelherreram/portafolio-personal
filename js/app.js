baf83b9620c3319b728fb993000060c683852239
axelherreram <aherreram22@miumg.edu.gt>
2024-10-07 20:48:24 -0600

    Imagenes y ajuste de estilos

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-// Script para manejar el botón de abrir/cerrar menú
+// Manejar el botón de abrir/cerrar menú
 const menuToggle = document.getElementById("menu-toggle");
 const mobileMenu = document.getElementById("mobile-menu");
 
@@ -10,12 +10,11 @@ menuToggle.addEventListener('click', function () {
     mobileMenu.classList.toggle('hidden');
 });
 
-// Función para cerrar el menú
+// Función para cerrar el menú cuando se hace clic en un enlace del menú responsivo
 function closeMenu() {
     mobileMenu.classList.add('hidden');
 }
 
-// Agregar el evento de clic para cada enlace del menú responsivo
 navLinks.forEach(link => {
     link.addEventListener('click', closeMenu);
 });
@@ -25,23 +24,23 @@ const sections = document.querySelectorAll("section");
 const navbarLinks = document.querySelectorAll(".nav-link");
 
 window.addEventListener("scroll", () => {
-  let current = "";
+    let current = "";
 
-  sections.forEach((section) => {
-    const sectionTop = section.offsetTop;
-    const sectionHeight = section.clientHeight;
+    sections.forEach((section) => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
 
-    if (window.scrollY >= sectionTop - sectionHeight / 3) {
-      current = section.getAttribute("id");
-    }
-  });
+        if (window.scrollY >= sectionTop - sectionHeight / 3) {
+            current = section.getAttribute("id");
+        }
+    });
 
-  navbarLinks.forEach((link) => {
-    link.classList.remove("text-white", "bg-white");
-    if (link.getAttribute("data-target") === current) {
-      link.classList.add("text-[#38bdf8]", "bg-white");
-    }
-  });
+    navbarLinks.forEach((link) => {
+        link.classList.remove("text-white", "bg-white");
+        if (link.getAttribute("data-target") === current) {
+            link.classList.add("text-[#38bdf8]", "bg-white");
+        }
+    });
 });
 
 // Funcionalidad para las animaciones de fadeIn
@@ -67,45 +66,42 @@ function checkFadeIn() {
     });
 }
 
-// Comprobar el scroll
+// Comprobar el scroll para las animaciones de fadeIn
 window.addEventListener('scroll', checkFadeIn);
-
-// Comprobar el scroll al cargar la página
 window.addEventListener('load', checkFadeIn);
 
 // Cambiar la clase del navbar al hacer scroll
 window.addEventListener("scroll", function () {
-  const navbar = document.getElementById("navbar");
-  const sectionProyectos = document.getElementById("section-proyectos");
-  const triggerHeight = sectionProyectos.offsetTop - 50; // Ajusta este valor según lo necesites
+    const navbar = document.getElementById("navbar");
+    const sectionProyectos = document.getElementById("section-proyectos");
+    const triggerHeight = sectionProyectos.offsetTop - 50; // Ajusta este valor según lo necesites
 
-  if (window.scrollY >= triggerHeight) {
-      navbar.classList.remove("md:bg-transparent");
-      navbar.classList.add("bg-[#0f172a]"); // Cambia al color deseado
-  } else {
-      navbar.classList.remove("bg-[#0f172a]");
-      navbar.classList.add("md:bg-transparent");
-  }
+    if (window.scrollY >= triggerHeight) {
+        navbar.classList.remove("md:bg-transparent");
+        navbar.classList.add("bg-[#0f172a]"); // Cambia al color deseado
+    } else {
+        navbar.classList.remove("bg-[#0f172a]");
+        navbar.classList.add("md:bg-transparent");
+    }
 });
 
-// Seleccionamos todos los enlaces de navegación con anclaje
+// Desplazamiento suave al hacer clic en enlaces del navbar
 const navLinksa = document.querySelectorAll('.nav-link');
 
 navLinksa.forEach(link => {
-  link.addEventListener('click', function (e) {
-    e.preventDefault(); // Prevenimos el comportamiento predeterminado
+    link.addEventListener('click', function (e) {
+        e.preventDefault(); 
 
-    const targetId = this.getAttribute('href'); // Obtenemos el id del destino
-    const targetSection = document.querySelector(targetId);
+        const targetId = this.getAttribute('href');
+        const targetSection = document.querySelector(targetId);
 
-    // Calculamos la posición del destino con un pequeño ajuste para el navbar
-    const offset = 70; // Altura de tu navbar
-    const elementPosition = targetSection.offsetTop - offset;
+        const offset = 80; 
+        const elementPosition = targetSection.offsetTop - offset;
 
-    // Desplazamiento suave hasta la sección con el ajuste
-    window.scrollTo({
-      top: elementPosition,
-      behavior: 'smooth' // Desplazamiento suave
+        // Desplazamiento suave hasta la sección con el ajuste
+        window.scrollTo({
+            top: elementPosition,
+            behavior: 'smooth'
+        });
     });
-  });
 });
